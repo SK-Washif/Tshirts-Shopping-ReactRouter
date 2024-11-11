451dfbe157068ad9e0b176ab1a739e8e7ab2e39e
SK-Washif <washiffishal@gmail.com>
2024-11-12 05:59:00 +0600

    Home page of Tshirts Shopping

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+import Main from './components/Layout/Main.jsx';
+import OrderReview from './components/OrderReview/OrderReview.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children:[
+      {
+        path: '/',
+        element: <Home></Home>,
+        loader: () => fetch('tshirts.json')
+      },
+      {
+        path: 'review',
+        element: <OrderReview></OrderReview>
+      }
+    ]
+  }
+])
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
